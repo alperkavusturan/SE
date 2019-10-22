25f1e2d4ec34c6df8fb1d9fbe3938605be683353
Alper Kavuşturan <bilgi@alperkavusturan.com>
2019-10-22 08:07:44 +0300

    Dashboard.Web creation

---
+++ Dashboard.Web/Data/ArtistService/ArtistService.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+
+namespace Dashboard.Web.Data.Services
+{
+    public class ArtistService
+    {
+        public TResponse CallService<TRequest, TResponse>(TRequest serviceRequest, string endpoint, Method method)
+        {
+            try
+            {
+                var client = new RestClient(string.Concat("http://176.53.34.37:54685/artist-service/", endpoint));
+                var request = new RestRequest(method);
+
+                var requestBody = JsonConvert.SerializeObject(serviceRequest);
+
+                var auth = new Auth { Username = "abc", Password = "123" };
+                var token = new AuthService().GetToken(auth);
+
+                request.AddParameter("application/json", requestBody, ParameterType.RequestBody);
+                request.AddHeader("Authorization", string.Concat("Bearer ", token.access_token));
+
+                var response = client.Execute(request);
+
+                return JsonConvert.DeserializeObject<TResponse>(response.Content);
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
+
+        }
+    }
+}
---
+++ Dashboard.Web/Data/AuthService/Token.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dashboard.Web.Data.Services
+{
+    public class Token
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+    }
+}
